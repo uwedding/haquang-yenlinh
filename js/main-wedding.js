9b0a9f1e34dc9754a59e0b9f06f035e93fbf8557
DESKTOP-90KR9TR\9999 <nguyenvancuong.tm@gmail.com>
2025-10-18 14:00:51 +0700

    update code

---
+++ js/main-wedding.js
@@ -0,0 +1,25 @@
+// AOS
+
+//AOS ANIMATION
+AOS.init({
+    once: false,
+    duration: 1200,
+});
+
+// SCROLLREVEA
+var swiper1 = new Swiper(".album-slide", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
